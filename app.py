b5a3984e09c833efb43a9661668a6b966ca6b88b
Atharvi Babhare <tanujababhare@gmail.com>
2026-04-16 13:36:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 st.title("💍 Wedding Data Analysis Dashboard")
 
 # Load data
-df = pd.read_csv("wedding_cost(1).ipynb")
+df = pd.read_csv("Indian_Weddings_.csv")
 
 # CLEAN COLUMNS
 df.columns = df.columns.str.strip()
